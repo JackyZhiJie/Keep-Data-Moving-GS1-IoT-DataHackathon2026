bc582c6cd2951752e429c5fc7ddc5e9ac93cc566
Jacky <1155158941@link.cuhk.edu.hk>
2026-03-28 10:52:11 +0800

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Keep-Data-Moving-GS1-IoT-DataHackathon2026/",
   plugins: [react()],
 });
